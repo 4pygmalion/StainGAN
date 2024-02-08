878d69311598a24cedef2b6704575acf9b5b6964
4pygmalion <4pygmalion@naver.com>
2024-02-08 12:47:32 +0900

    hotfix for version update

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -14,7 +14,7 @@ import sys
 
 class CycleGANModel(BaseModel):
     def name(self):
-        return 'CycleGANModel'
+        return "CycleGANModel"
 
     def initialize(self, opt):
         BaseModel.initialize(self, opt)
@@ -26,35 +26,65 @@ class CycleGANModel(BaseModel):
         # The naming conversion is different from those used in the paper
         # Code (paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
 
-        self.netG_A = networks.define_G(opt.input_nc, opt.output_nc,
-                                        opt.ngf, opt.which_model_netG, opt.norm, not opt.no_dropout, opt.init_type,
-                                        self.gpu_ids)
-        self.netG_B = networks.define_G(opt.output_nc, opt.input_nc,
-                                        opt.ngf, opt.which_model_netG, opt.norm, not opt.no_dropout, opt.init_type,
-                                        self.gpu_ids)
+        self.netG_A = networks.define_G(
+            opt.input_nc,
+            opt.output_nc,
+            opt.ngf,
+            opt.which_model_netG,
+            opt.norm,
+            not opt.no_dropout,
+            opt.init_type,
+            self.gpu_ids,
+        )
+        self.netG_B = networks.define_G(
+            opt.output_nc,
+            opt.input_nc,
+            opt.ngf,
+            opt.which_model_netG,
+            opt.norm,
+            not opt.no_dropout,
+            opt.init_type,
+            self.gpu_ids,
+        )
 
         if self.isTrain:
             use_sigmoid = opt.no_lsgan
-            self.netD_A = networks.define_D(opt.output_nc, opt.ndf,
-                                            opt.which_model_netD,
-                                            opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, self.gpu_ids)
-            self.netD_B = networks.define_D(opt.input_nc, opt.ndf,
-                                            opt.which_model_netD,
-                                            opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, self.gpu_ids)
+            self.netD_A = networks.define_D(
+                opt.output_nc,
+                opt.ndf,
+                opt.which_model_netD,
+                opt.n_layers_D,
+                opt.norm,
+                use_sigmoid,
+                opt.init_type,
+                self.gpu_ids,
+            )
+            self.netD_B = networks.define_D(
+                opt.input_nc,
+                opt.ndf,
+                opt.which_model_netD,
+                opt.n_layers_D,
+                opt.norm,
+                use_sigmoid,
+                opt.init_type,
+                self.gpu_ids,
+            )
         if not self.isTrain or opt.continue_train:
             which_epoch = opt.which_epoch
-            self.load_network(self.netG_A, 'G_A', which_epoch)
-            self.load_network(self.netG_B, 'G_B', which_epoch)
+            self.load_network(self.netG_A, "G_A", which_epoch)
+            self.load_network(self.netG_B, "G_B", which_epoch)
             if self.isTrain:
-                self.load_network(self.netD_A, 'D_A', which_epoch)
-                self.load_network(self.netD_B, 'D_B', which_epoch)
+                self.load_network(self.netD_A, "D_A", which_epoch)
+                self.load_network(self.netD_B, "D_B", which_epoch)
 
         if self.isTrain:
             self.old_lr = opt.lr
             self.fake_A_pool = ImagePool(opt.pool_size)
             self.fake_B_pool = ImagePool(opt.pool_size)
             # define loss functions
-            self.criterionGAN = networks.GANLoss(use_lsgan=not opt.no_lsgan, tensor=self.Tensor)
+            self.criterionGAN = networks.GANLoss(
+                use_lsgan=not opt.no_lsgan, tensor=self.Tensor
+            )
             self.criterionCycle = torch.nn.L1Loss()
             self.criterionIdt = torch.nn.L1Loss()
             # gradient loss as per aicha
@@ -62,10 +92,17 @@ class CycleGANModel(BaseModel):
                 self.criterionAicha = torch.nn.L1Loss()
 
             # initialize optimizers
-            self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()),
-                                                lr=opt.lr, betas=(opt.beta1, 0.999))
-            self.optimizer_D_A = torch.optim.Adam(self.netD_A.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-            self.optimizer_D_B = torch.optim.Adam(self.netD_B.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_G = torch.optim.Adam(
+                itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()),
+                lr=opt.lr,
+                betas=(opt.beta1, 0.999),
+            )
+            self.optimizer_D_A = torch.optim.Adam(
+                self.netD_A.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999)
+            )
+            self.optimizer_D_B = torch.optim.Adam(
+                self.netD_B.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999)
+            )
 
             self.optimizers = []
             self.schedulers = []
@@ -84,15 +121,15 @@ class CycleGANModel(BaseModel):
         # print('-----------------------------------------------')
 
     def set_input(self, input):
-        AtoB = self.opt.which_direction == 'AtoB'
-        input_A = input['A' if AtoB else 'B']
-        input_B = input['B' if AtoB else 'A']
+        AtoB = self.opt.which_direction == "AtoB"
+        input_A = input["A" if AtoB else "B"]
+        input_B = input["B" if AtoB else "A"]
         if len(self.gpu_ids) > 0:
-            input_A = input_A.cuda(self.gpu_ids[0], async=True)
-            input_B = input_B.cuda(self.gpu_ids[0], async=True)
+            input_A = input_A.cuda(self.gpu_ids[0])
+            input_B = input_B.cuda(self.gpu_ids[0])
         self.input_A = input_A
         self.input_B = input_B
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        self.image_paths = input["A_paths" if AtoB else "B_paths"]
 
     def forward(self):
         self.real_A = Variable(self.input_A)
@@ -129,14 +166,15 @@ class CycleGANModel(BaseModel):
     def backward_D_A(self):
         fake_B = self.fake_B_pool.query(self.fake_B)
         loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, fake_B)
-        self.loss_D_A = loss_D_A.data[0]
+        self.loss_D_A = loss_D_A.item()
 
     def backward_D_B(self):
         fake_A = self.fake_A_pool.query(self.fake_A)
         loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, fake_A)
-        self.loss_D_B = loss_D_B.data[0]
+        self.loss_D_B = loss_D_B.item()
 
     def backward_G(self):
+
         lambda_idt = self.opt.identity
         lambda_A = self.opt.lambda_A
         lambda_B = self.opt.lambda_B
@@ -148,11 +186,10 @@ class CycleGANModel(BaseModel):
             # G_B should be identity if real_A is fed.
             idt_B = self.netG_B(self.real_A)
             loss_idt_B = self.criterionIdt(idt_B, self.real_A) * lambda_A * lambda_idt
-
             self.idt_A = idt_A.data
             self.idt_B = idt_B.data
-            self.loss_idt_A = loss_idt_A.data[0]
-            self.loss_idt_B = loss_idt_B.data[0]
+            self.loss_idt_A = loss_idt_A.item()
+            self.loss_idt_B = loss_idt_B.item()
         else:
             loss_idt_A = 0
             loss_idt_B = 0
@@ -203,8 +240,8 @@ class CycleGANModel(BaseModel):
 
             realB_img = util.tensor2im(self.real_B.data)
             recB_img = util.tensor2im(rec_B.data)
-            plt.imshow(realB_img);
-            plt.imshow(recB_img);
+            plt.imshow(realB_img)
+            plt.imshow(recB_img)
             plt.show()
 
             sys.exit()
@@ -217,17 +254,34 @@ class CycleGANModel(BaseModel):
 
             # wx = Variable(torch.from_numpy(), requires_grad=True)
             wx = grad_times_real
-            print('grad_times_real', grad_times_real)
+            print("grad_times_real", grad_times_real)
             sys.exit()
-            wg = Variable(torch.from_numpy(np.multiply(gradient_input, recB_img)), requires_grad=True)
+            wg = Variable(
+                torch.from_numpy(np.multiply(gradient_input, recB_img)),
+                requires_grad=True,
+            )
             loss_Aicha_B = self.criterionAicha(wx, wg) * self.opt.lambda_Aicha
 
             # aicha combined loss
 
-            loss_G = loss_G_A + loss_G_B + loss_Aicha_A + loss_Aicha_B + loss_idt_A + loss_idt_B
+            loss_G = (
+                loss_G_A
+                + loss_G_B
+                + loss_Aicha_A
+                + loss_Aicha_B
+                + loss_idt_A
+                + loss_idt_B
+            )
         else:
             # combined loss
-            loss_G = loss_G_A + loss_G_B + loss_cycle_A + loss_cycle_B + loss_idt_A + loss_idt_B
+            loss_G = (
+                loss_G_A
+                + loss_G_B
+                + loss_cycle_A
+                + loss_cycle_B
+                + loss_idt_A
+                + loss_idt_B
+            )
 
         loss_G.backward()
 
@@ -236,17 +290,17 @@ class CycleGANModel(BaseModel):
         self.rec_A = rec_A.data
         self.rec_B = rec_B.data
 
-        self.loss_G_A = loss_G_A.data[0]
-        self.loss_G_B = loss_G_B.data[0]
+        self.loss_G_A = loss_G_A.item()
+        self.loss_G_B = loss_G_B.item()
 
         if self.opt.aicha_loss:
             # AichaLoss
-            self.loss_Aicha_A = loss_Aicha_A.data[0]
-            self.loss_Aicha_B = loss_Aicha_B.data[0]
+            self.loss_Aicha_A = loss_Aicha_A.item()
+            self.loss_Aicha_B = loss_Aicha_B.item()
         else:
             # cycleLoss
-            self.loss_cycle_A = loss_cycle_A.data[0]
-            self.loss_cycle_B = loss_cycle_B.data[0]
+            self.loss_cycle_A = loss_cycle_A.item()
+            self.loss_cycle_B = loss_cycle_B.item()
 
     #
     # def get_gradient(self, img):
@@ -271,15 +325,23 @@ class CycleGANModel(BaseModel):
         self.optimizer_D_B.step()
 
     def get_current_errors(self):
-        ret_errors = OrderedDict([('D_A', self.loss_D_A), ('G_A', self.loss_G_A), ('Cyc_A', self.loss_cycle_A),
-                                  ('D_B', self.loss_D_B), ('G_B', self.loss_G_B), ('Cyc_B', self.loss_cycle_B)])
+        ret_errors = OrderedDict(
+            [
+                ("D_A", self.loss_D_A),
+                ("G_A", self.loss_G_A),
+                ("Cyc_A", self.loss_cycle_A),
+                ("D_B", self.loss_D_B),
+                ("G_B", self.loss_G_B),
+                ("Cyc_B", self.loss_cycle_B),
+            ]
+        )
         # aicha
         # ret_errors = OrderedDict([('D_A', self.loss_D_A), ('G_A', self.loss_G_A), ('Aicha_A', self.loss_cycle_A),
         #                           ('D_B', self.loss_D_B), ('G_B', self.loss_G_B), ('Aicha_B', self.loss_cycle_B)])
 
         if self.opt.identity > 0.0:
-            ret_errors['idt_A'] = self.loss_idt_A
-            ret_errors['idt_B'] = self.loss_idt_B
+            ret_errors["idt_A"] = self.loss_idt_A
+            ret_errors["idt_B"] = self.loss_idt_B
         return ret_errors
 
     def get_current_visuals(self):
@@ -289,15 +351,23 @@ class CycleGANModel(BaseModel):
         real_B = util.tensor2im(self.input_B)
         fake_A = util.tensor2im(self.fake_A)
         rec_B = util.tensor2im(self.rec_B)
-        ret_visuals = OrderedDict([('real_A', real_A), ('fake_B', fake_B), ('rec_A', rec_A),
-                                   ('real_B', real_B), ('fake_A', fake_A), ('rec_B', rec_B)])
+        ret_visuals = OrderedDict(
+            [
+                ("real_A", real_A),
+                ("fake_B", fake_B),
+                ("rec_A", rec_A),
+                ("real_B", real_B),
+                ("fake_A", fake_A),
+                ("rec_B", rec_B),
+            ]
+        )
         if self.opt.isTrain and self.opt.identity > 0.0:
-            ret_visuals['idt_A'] = util.tensor2im(self.idt_A)
-            ret_visuals['idt_B'] = util.tensor2im(self.idt_B)
+            ret_visuals["idt_A"] = util.tensor2im(self.idt_A)
+            ret_visuals["idt_B"] = util.tensor2im(self.idt_B)
         return ret_visuals
 
     def save(self, label):
-        self.save_network(self.netG_A, 'G_A', label, self.gpu_ids)
-        self.save_network(self.netD_A, 'D_A', label, self.gpu_ids)
-        self.save_network(self.netG_B, 'G_B', label, self.gpu_ids)
-        self.save_network(self.netD_B, 'D_B', label, self.gpu_ids)
+        self.save_network(self.netG_A, "G_A", label, self.gpu_ids)
+        self.save_network(self.netD_A, "D_A", label, self.gpu_ids)
+        self.save_network(self.netG_B, "G_B", label, self.gpu_ids)
+        self.save_network(self.netD_B, "D_B", label, self.gpu_ids)
